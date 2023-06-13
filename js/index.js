012c0102366c2784a226a8432761db6d7a3041b5
Gabriel Miranda <92379320+GaboMiranda1979@users.noreply.github.com>
2023-06-13 13:57:02 -0300

    V 1.0

---
+++ js/index.js
@@ -0,0 +1,18 @@
+(function() {
+    AOS.init(); 
+}());
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
